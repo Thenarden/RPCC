96e8229380a4839660f0aafd08f716b4aa0f5e21
Fabian Schreier <fabian.p.schreier@web.de>
2013-01-13 01:04:44 +0100

    Added (unfinished) calculation of binary operator type.

--- RPCC/AST/Operators/IBinaryOperator.cs
+++ RPCC/AST/Operators/IBinaryOperator.cs
@@ -9,6 +9,32 @@ namespace RPCC.AST.Operators
 {
 	abstract class IBinaryOperator : IRightValue
 	{
+
+		public override ITypeSpecifier Type
+		{
+			get
+			{
+				if (this.FirstOperand.Type.Equals(this.SecondOperand.Type))
+					return this.FirstOperand.Type.Clone(this); // Whahaha, simple and nice...
+
+				// TODO: Add Unsigned double here at top 
+				if (OneOperandHasType(AtomicTypeSpecifier.Double(this)))
+					return AtomicTypeSpecifier.Double(this);
+				else if (OneOperandHasType(AtomicTypeSpecifier.Float(this)))
+					return AtomicTypeSpecifier.Float(this);
+				else if (OneOperandHasType(AtomicTypeSpecifier.ULong(this)))
+					return AtomicTypeSpecifier.ULong(this);
+				else if ((OneOperandHasType(AtomicTypeSpecifier.Long(this))) && (OneOperandIsUnsigned())) // Cause ULong is checked before, the long operand can't be unsigned...
+					return AtomicTypeSpecifier.ULong(this); // TODO: Use long, if sizeof(long) > sizeof(unsigned)
+				else if (OneOperandHasType(AtomicTypeSpecifier.Long(this)))
+					return AtomicTypeSpecifier.Long(this);
+				//else if (OneOperandIsUnsigned()) // TODO: unsigned...
+				//	return AtomicTypeSpecifier.Float(this); // TODO: Return the right one?
+				else
+					return AtomicTypeSpecifier.Int(this);
+			}
+		}
+
 		public IRightValue FirstOperand
 		{
 			get;
@@ -62,6 +88,17 @@ namespace RPCC.AST.Operators
 					Input = temp;
 					throw new NotImplementedException();
 			}
+		}
+
+		private bool OneOperandHasType (AtomicTypeSpecifier type)
+		{
+			return type.Equals(FirstOperand.Type) || type.Equals(SecondOperand.Type);
+		}
+		private bool OneOperandIsUnsigned()
+		{
+			bool firstUnsigned = (FirstOperand.Type is AtomicTypeSpecifier) ? (FirstOperand.Type as AtomicTypeSpecifier).Unsigned : false;
+			bool secondUnsigned = (SecondOperand.Type is AtomicTypeSpecifier) ? (SecondOperand.Type as AtomicTypeSpecifier).Unsigned : false;
+			return firstUnsigned || secondUnsigned;
 		}
 	}
 }
