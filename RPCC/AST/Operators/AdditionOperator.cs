9bd3eb30b1a0c872571fe3676379d01d6477fa6a
Fabian Schreier <fabian.p.schreier@web.de>
2013-01-13 01:04:04 +0100

    Added first (unfinished) implementation of AdditionOperator

---
+++ RPCC/AST/Operators/AdditionOperator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPCC.AST.Operators
+{
+	class AdditionOperator : IBinaryOperator
+	{
+
+		public AdditionOperator (ISyntaxNode parent, IRightValue firstOperand, IRightValue secondOperand)
+			: base (parent, firstOperand, secondOperand)
+		{}
+
+		public override byte[] Compile()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
